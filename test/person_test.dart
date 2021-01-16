4aa3e9d7b8dc4bc58cccefcb5c44281661407dff
Leja Aprianza <72089263+lejaaprianza@users.noreply.github.com>
2021-01-16 12:10:30 +0700

    Add files via upload

---
+++ test/person_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hello_world/person.dart';
+
+void main() {
+  group("Mengecek class Person", () {
+    Person p;
+    p = Person();
+    test("Inisialisasi Objek Person", () {
+      expect(p.name, equals("no name"));
+      expect(p.age, equals(0));
+    });
+
+    test("Age harus positif", () {
+      p.age = -5;
+      expect(p.age, isPositive);
+    });
+
+    test(description, body)
+  });
+}
